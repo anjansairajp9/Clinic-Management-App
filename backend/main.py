85033e0f77c7d9971c409ccbd21c54e1d2da81cc
Anjan Sairaj <anjansairaj@gmail.com>
2026-06-09 22:02:49 +0530

    Added CORS to talk to frontend localhost

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from backend.routes import (
@@ -13,6 +14,15 @@ from backend.routes import (
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
