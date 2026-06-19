6d5c7e0ed9631647acf79147932850ec1a590b3b
Anjan Sairaj <anjansairaj@gmail.com>
2026-06-19 21:54:24 +0530

    Tested, cleaned up and made sure everything works and complete frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,11 +5,13 @@ import HeroSection from "@/components/shared/HeroSection";
 import FeaturesSection from "@/components/shared/FeaturesSection";
 
 export default function HomePage() {
-  return (
-    <main className="overflow-hidden">
-      <Navbar />
-      <HeroSection />
-      <FeaturesSection />
-    </main>
-  );
+	return (
+		<main className="overflow-hidden">
+			<Navbar />
+
+			<HeroSection />
+
+			<FeaturesSection />
+		</main>
+	);
 }
