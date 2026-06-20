dc50006e4232a59f457e6dbdc76f44b484ef2a33
Anjan Sairaj <anjansairaj@gmail.com>
2026-06-20 23:00:52 +0530

    Update CORS for production domain

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://clinsyst.in",
+        "https://www.clinsyst.in"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
